8173ea1732694fd78e35a46df96b9840168911c2
辻真緒 <>
2019-08-06 12:37:32 +0800

    カルーセル機能追加

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,24 +14,82 @@
         {{-- bootstrapの追加 --}}
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
+
     </head>
     <body data-gr-c-s-loaded="true" cz-shortcut-listen="true">
-       
-  <!-- Hero -->
-    <!-- Just an image -->
-        {{-- <nav class="navbar navbar-light bg-light">
-          <a class="navbar-brand" href="#">
-            <img src="/img/W1GP11th.png" width="50" height="30" alt="">
-          </a>
-        </nav> --}}
-        {{--  --}}
 
         <section class="et-hero-tabs">
-            <h1>ここにカルーセル</h1>
-            <h3>Sliding content with sticky tab nav</h3>
+
+            {{-- カルーセル --}}
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img class="d-block w-100" src="/img/carousel/topImage1.jpg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="/img/carousel/topImage2.jpg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="/img/carousel/topImage3.jpg" alt="Third slide">
+                </div>
+              </div>
+              <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
+
+            {{-- カルーセルスライダー --}}
+{{--             <div class="worksslider inside">
+                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" data-interval="5000">
+                  <!-- Indicators -->
+                  <ol class="carousel-indicators">
+                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                  </ol>
+
+                  <!-- Wrapper for slides -->
+                  <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                      <img src="/img/carousel/topImage1" alt="">
+                      <div class="carousel-caption">
+                        キャプション
+                      </div>
+                    </div>
+                    <div class="item">
+                      <img src="/img/carousel/topImage2" alt="">
+                      <div class="carousel-caption">
+                        キャプション
+                      </div>
+                    </div>
+                    <div class="item">
+                      <img src="/img/carousel/topImage3" alt="">
+                      <div class="carousel-caption">
+                        キャプション
+                      </div>
+                    </div>
+                  </div>
+
+                  <!-- Controls -->
+                  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </div>
+            </div> --}}
 
 
-            {{-- ↓↓↓↓↓↓↓ここから↓↓↓↓↓↓↓↓↓↓ --}}
+
+            {{-- ナブバー リンク --}}
             <div class="navbar-link">
 
                 <div class="header-left">
